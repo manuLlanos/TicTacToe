9fd29404361f4166da34184bae6fff5b56612d11
Manuel Llanos <manuelllanos_8@hotmail.com>
2017-04-11 19:59:49 -0300

    Made random.h an actual header

--- headers/Random.h
+++ headers/Random.h
@@ -9,19 +9,10 @@
 
 #define SEED srand( (unsigned)time(NULL))			//seed initialization
 
-unsigned Random(unsigned m, unsigned M)
-{
-	return m + rand()%(M-m);
-}
+unsigned Random(unsigned, unsigned );
 
-unsigned Random(unsigned M)
-{
-	return Random(0,M);
-}
+unsigned Random(unsigned);
 
-double RandomPorcentaje()				// return a percetage [0;1) with 4 precission digits
-{
-	return (double )Random(10000)/10000;
-}
+double RandomPorcentaje();
 
 #endif
